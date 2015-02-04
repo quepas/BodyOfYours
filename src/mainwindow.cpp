d08b4cf54d6b26b0afe25f27d60d37f9275f1d51
Quepas <oneinchman.dev@gmail.com>
2015-02-04 18:59:37 +0100

    Adding QVTKWidget connected with PCL-based visualizer.

--- src/mainwindow.cpp
+++ src/mainwindow.cpp
@@ -2,6 +2,19 @@
 #include "ui_mainwindow.h"
 
 #include <QFileSystemModel>
+#include <QDebug>
+
+#include <pcl/visualization/pcl_visualizer.h>
+#include <pcl/io/ply_io.h>
+
+#include <QVTKWidget.h>
+#include <vtkRenderer.h>
+#include <vtkRenderWindow.h>
+
+pcl::visualization::PCLVisualizer pviz ("PCL Visualizer", false);
+pcl::PointCloud<pcl::PointXYZ>::Ptr cloud_xyz (new pcl::PointCloud<pcl::PointXYZ>);
+pcl::PointCloud<pcl::PointXYZ>::Ptr ply_cloud (new pcl::PointCloud<pcl::PointXYZ>);
+bool ply_loaded;
 
 MainWindow::MainWindow(QWidget *parent)
   : QMainWindow(parent),
@@ -9,6 +22,11 @@ MainWindow::MainWindow(QWidget *parent)
 {
   ui->setupUi(this);
   SetupScansTree();
+  ply_loaded = false;
+  vtkSmartPointer<vtkRenderWindow> renderWindow = pviz.getRenderWindow();
+  ui->scansViewer->SetRenderWindow (renderWindow);
+  pviz.setupInteractor (ui->scansViewer->GetInteractor (), ui->scansViewer->GetRenderWindow ());
+  ui->scansViewer->show();
 }
 
 MainWindow::~MainWindow()
@@ -19,7 +37,7 @@ MainWindow::~MainWindow()
 void MainWindow::SetupScansTree()
 {
   QFileSystemModel* model = new QFileSystemModel;
-  QString data_root_path = QDir::currentPath() + "/data/";
+  QString data_root_path = QDir::currentPath() + "";
   model->setRootPath(data_root_path);
   QTreeView* tree = ui->scansTree;
   tree->setModel(model);
@@ -28,4 +46,18 @@ void MainWindow::SetupScansTree()
   tree->setColumnWidth(1, 50);
   tree->setColumnWidth(2, 70);
   tree->setColumnWidth(3, 100);
+}
+
+
+void MainWindow::on_scansTree_doubleClicked(const QModelIndex &index)
+{
+  QFileSystemModel* model = (QFileSystemModel*) ui->scansTree->model();
+  pcl::io::loadPLYFile(model->fileName(index).toStdString(), *ply_cloud);
+  pcl::PointCloud<pcl::PointXYZ>::ConstPtr ply_cloud2 (ply_cloud);
+  if (ply_loaded) {
+    pviz.updatePointCloud(ply_cloud2, "asd");
+  } else {
+     pviz.addPointCloud(ply_cloud2, "asd");
+  }
+  ply_loaded = true;
 }
--- src/mainwindow.h
+++ src/mainwindow.h
@@ -2,6 +2,7 @@
 #define MAINWINDOW_H
 
 #include <QMainWindow>
+#include <QModelIndex>
 
 namespace Ui {
 class MainWindow;
@@ -14,6 +15,9 @@ class MainWindow : public QMainWindow
 public:
   explicit MainWindow(QWidget *parent = 0);
   ~MainWindow();
+
+private slots:
+  void on_scansTree_doubleClicked(const QModelIndex& index);
 
 private:
   Ui::MainWindow *ui;
