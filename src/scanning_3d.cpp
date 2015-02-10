f5032823271cb2eb8b0ff18febababf698f877de
Quepas <oneinchman.dev@gmail.com>
2015-02-11 00:47:57 +0100

    Adding real-time loop for sensor's camera frame acquisition every 100ms.

--- src/scanning_3d.cpp
+++ src/scanning_3d.cpp
@@ -1,12 +1,19 @@
 #include "scanning_3d.h"
 
-Scanning3D::Scanning3D(Scanner3D* scanner3d)
-  : scanner_(scanner3d)
+Scanning3D::Scanning3D(Scanner3D* scanner3d, SensorViewer* viewer)
+  : scanner_(scanner3d),
+    viewer_(viewer)
 {
 
 }
 
 void Scanning3D::run()
 {
-  scanner_->GrabCamera();
+  bool is_running = true;
+  while(is_running) {
+    FrameData* frame = new FrameData();
+    scanner_->GrabCameraFrame(frame);
+    emit grabFrame(frame);
+    QThread::msleep(100);
+  }
 }
--- src/scanning_3d.h
+++ src/scanning_3d.h
@@ -1,17 +1,23 @@
 #pragma once
 
 #include "scanner_3d.h"
+#include "sensor_viewer.h"
 
 #include <QThread>
 
 class Scanning3D : public QThread
 {
+  Q_OBJECT
 public:
-  Scanning3D(Scanner3D* scanner3d);
+  Scanning3D(Scanner3D* scanner3d, SensorViewer* viewer);
 
 protected:
   void run();
 
+signals:
+  void grabFrame(FrameData* frame);
+
 private:
   Scanner3D* scanner_;
+  SensorViewer* viewer_;
 };
