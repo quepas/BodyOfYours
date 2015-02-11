9e8255d02edfb91daf743f73abd5d17562cc785b
Quepas <oneinchman.dev@gmail.com>
2015-02-11 16:34:53 +0100

    Adding SensorViewer for camera, depth and volume data.

--- src/scanningwindow.cpp
+++ src/scanningwindow.cpp
@@ -6,9 +6,22 @@ ScanningWindow::ScanningWindow(QWidget *parent) :
   ui(new Ui::ScanningWindow)
 {
   ui->setupUi(this);
+  camera_viewer_ = new SensorViewer(ui->scansCameraViewer);
+  depth_viewer_ = new SensorViewer(ui->scansDepthViewer);
+  volume_viewer_ = new SensorViewer(ui->scansVolumeViewer);
 }
 
 ScanningWindow::~ScanningWindow()
 {
   delete ui;
+}
+
+void ScanningWindow::on_startScanButton_clicked()
+{
+
+}
+
+void ScanningWindow::on_stopScanButton_clicked()
+{
+
 }
--- src/scanningwindow.h
+++ src/scanningwindow.h
@@ -1,6 +1,8 @@
 #ifndef SCANNINGWINDOW_H
 #define SCANNINGWINDOW_H
 
+#include "sensor_viewer.h"
+
 #include <QDialog>
 
 namespace Ui {
@@ -15,8 +17,13 @@ public:
   explicit ScanningWindow(QWidget *parent = 0);
   ~ScanningWindow();
 
+private slots:
+  void on_startScanButton_clicked();
+  void on_stopScanButton_clicked();
+
 private:
   Ui::ScanningWindow *ui;
+  SensorViewer *camera_viewer_, *depth_viewer_, *volume_viewer_;
 };
 
 #endif // SCANNINGWINDOW_H
