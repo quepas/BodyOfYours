611290b2af1bfc4ff014c9af2dc046fe0c72544a
Quepas <oneinchman.dev@gmail.com>
2015-02-11 02:06:32 +0100

    Proper initialization for depth image structure.

--- src/reme_scanner_3d.cpp
+++ src/reme_scanner_3d.cpp
@@ -9,6 +9,7 @@ RemeScanner3D::RemeScanner3D()
   reme_sensor_open(context_, sensor_);
   reme_viewer_create_image(context_, "Image viewer", &img_viewer_);
   reme_image_create(context_, &image_aux_);
+  reme_image_create(context_, &image_depth_);
   reme_viewer_add_image(context_, img_viewer_, image_aux_);
 }
 
