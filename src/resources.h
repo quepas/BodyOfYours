2efa968a7ccda9f7c8c4efd664b15c8b8963336d
Quepas <oneinchman.dev@gmail.com>
2015-02-17 01:24:44 +0100

    Adding female/male icons.

--- src/resources.h
+++ src/resources.h
@@ -8,6 +8,8 @@ namespace Resources {
   static const QString ICON_ERROR = "gui/icons/error.ico";
   static const QString ICON_SYNC = "gui/icons/sync.ico";
   static const QString ICON_WARNING = "gui/icons/warning.ico";
+  static const QString ICON_FEMALE = "gui/icons/female.ico";
+  static const QString ICON_MALE = "gui/icons/male.ico";
 
   static const QString SCANS_DATA_PATH = QDir::currentPath() + "/data";
 }
