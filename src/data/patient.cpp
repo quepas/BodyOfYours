f1bc26c647442132448e5030e82eb73353da19d8
Quepas <oneinchman.dev@gmail.com>
2015-03-09 13:48:25 +0100

    Adding additional fields to Patient class, defualt constructor.

--- src/data/patient.cpp
+++ src/data/patient.cpp
@@ -1,5 +1,11 @@
 #include "patient.h"
 
+Patient::Patient()
+  : id_("none"), name_("none"), surname_("none"), additional_info_("none")
+{
+
+}
+
 Patient::Patient(QString name, QString surname, QString additional_info)
   : name_(name), surname_(surname), additional_info_(additional_info)
 {
--- src/data/patient.h
+++ src/data/patient.h
@@ -2,24 +2,34 @@
 
 #include <QString>
 
+enum Sex_
+{
+  FEMALE_,
+  MALE_
+};
+
 class Patient
 {
 public:
+  Patient();
   Patient(QString name, QString surname, QString additional_info);
 
   QString id() { return id_; }
   QString name() { return name_; }
   QString surname() { return surname_; }
-  QString additional_info() { additional_info_; }
+  QString additional_info() { return additional_info_; }
+  Sex_ sex() { return sex_; }
 
   void setId(QString id) { id_ = id; }
   void setName(QString name) { name_ = name; }
   void setSurname(QString surname) { surname_ = surname; }
-  QString setAdditionalInfo(QString additional_info) { additional_info_ = additional_info; }
+  void setAdditionalInfo(QString additional_info) { additional_info_ = additional_info; }
+  void setSex(Sex_ sex) { sex_ = sex; }
 
 private:
   QString id_;
   QString name_;
   QString surname_;
   QString additional_info_;
+  Sex_ sex_;
 };
