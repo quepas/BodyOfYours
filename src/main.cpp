e8f1e1d24bc3b19009cbefe8b8d529829a4f0cde
Quepas <oneinchman.dev@gmail.com>
2015-09-02 17:30:31 +0200

    Removing tabs.

--- src/main.cpp
+++ src/main.cpp
@@ -2,15 +2,12 @@
 
 #include <QtWidgets/QApplication>
 
-
 int main(int argc, char *argv[])
 {
 	QApplication app(argc,argv);
 	app.setOrganizationName("JustAStartup");
 	app.setApplicationName("BodyOfYours");
-
-	MainWindow mw;
-	mw.show();
-
-	return app.exec();
+  MainWindow mw;
+  mw.show();
+  return app.exec();
 }
