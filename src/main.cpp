342f330b84cf1e7364433b9147fb072c86b36e5f
Quepas <oneinchman.dev@gmail.com>
2015-09-02 14:30:34 +0200

    Changing app name.

--- src/main.cpp
+++ src/main.cpp
@@ -6,8 +6,8 @@
 int main(int argc, char *argv[])
 {
 	QApplication app(argc,argv);
-	app.setOrganizationName("ImFusion");
-	app.setApplicationName("RecFusionSDKTest");
+	app.setOrganizationName("JustAStartup");
+	app.setApplicationName("BodyOfYours");
 
 	MainWindow mw;
 	mw.show();
