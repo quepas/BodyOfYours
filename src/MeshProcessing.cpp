136e44015f42eecc0a492c8c24dc07842a581c37
Quepas <oneinchman.dev@gmail.com>
2015-11-12 00:56:01 +0100

    Mesh processing functions, opening PLY with VCG.

---
+++ src/MeshProcessing.cpp
@@ -0,0 +1,23 @@
+#include "MeshProcessing.h"
+
+#include <QDebug>
+#include <wrap/io_trimesh/import.h>
+
+using vcg::tri::io::Importer;
+using vcg::tri::Clean;
+
+void openMesh(QString filename, CMesh& out, bool clean_data /*= false*/)
+{
+  int err = Importer<CMesh>::Open(out, qPrintable(filename));
+  if (err) {
+    qDebug() << "[Error] Error in reading " << filename << ":" << Importer<CMesh>::ErrorMsg(err);
+    if (Importer<CMesh>::ErrorCritical(err))
+      qDebug() << "[Critical] Serious error.";
+  }
+  qDebug() << "[Info] Read mesh " << filename;
+  if (clean_data){
+    int dup = Clean<CMesh>::RemoveDuplicateVertex(out);
+    int unref = Clean<CMesh>::RemoveUnreferencedVertex(out);
+    qDebug() << "Removed " << dup << " duplicate and " << unref << " unreferenced vertices from mesh " << filename;
+  }
+}
